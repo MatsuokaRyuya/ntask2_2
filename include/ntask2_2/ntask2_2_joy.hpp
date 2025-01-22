10efed67a483da60d55b00ba0e4df122f9c08048
MatsuokaRyuya <matsuoka.ryuya.p4@dc.tohoku.ac.jp>
2025-01-22 18:45:37 +0900

    Update ntask2_2_joy.hpp

--- include/ntask2_2/ntask2_2_joy.hpp
+++ include/ntask2_2/ntask2_2_joy.hpp
@@ -2,11 +2,12 @@
 #define NTASK2_2_JOY_HPP
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/float64.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include <numeric>
 #include <string>
 #include <vector>
-#include <numeric>
+#include "std_msgs/msg/float64.hpp"
 
 class JoySubscriber : public rclcpp::Node 
 {
@@ -16,14 +17,8 @@ public:
 private:
     void joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg);
 
-    // Joyトピックの購読
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-
-    // 速度制御用パブリッシャー
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr speed_publisher_;
-
-    // 位置制御用パブリッシャー
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr position_publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_; // Joyトピックの購読
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;      // Dynamixel制御用パブリッシャー
 };
 
-#endif // NTASK2_2_JOY_HPP
+#endif
